868a431dadd91eec88fb165afdd61422c09b1b2f
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2026-01-19 13:13:31 +0500

    Create Q2.py

---
+++ LAB 1/Q2.py
@@ -0,0 +1,9 @@
+string_test = input("Enter any String you want: ")
+
+print("First Character: ", string_test[0])
+print("Last Character: ", string_test[len(string_test) - 1])        # Calculate total length of string -1 (to remove the string terminator)
+print("Length of String: ", len(string_test))
+print("Repeated Twice String: ", *string_test * 2)                  # Prnt string two times
+
+count = 0
+while (len)
